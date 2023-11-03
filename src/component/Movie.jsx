4d365a5fd104fe61f1c65b2036f7953a10c0a5eb
MAX POLLOCK <mcpollock00@gmail.com>
2023-11-03 14:22:45 +0000

    tweaked movies section

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -6,8 +6,10 @@ export default function RenderMovie({ weather }) {
         {weather.map((data) => (
           <div key={data.date}>
             <div className="resultsweath">
-              <h4>Title: {data.title}</h4>
-              <h4>Overview: {data.overview}</h4>
+              <h4>Title:</h4>
+              <h4>{data.title}</h4>
+              <h4>Overview:</h4>
+              <h4>{data.overview}</h4>
               <h4>Popularity: {data.popularity}</h4>
             </div>
           </div>
